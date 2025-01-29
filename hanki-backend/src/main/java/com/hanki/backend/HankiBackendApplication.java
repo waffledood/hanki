bbd5b4229050f77a3b94e6c2f0631dcacd0b69cc
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-01-29 21:11:55 +0800

    Initialize hanki-backend Spring Boot project

---
+++ hanki-backend/src/main/java/com/hanki/backend/HankiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hanki.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankiBackendApplication.class, args);
+	}
+
+}
