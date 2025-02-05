b4593d4a91979ffd59e1692451c0246f16ef4327
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-05 23:09:49 +0800

    Define testGetDeckById test

--- hanki-backend/src/test/java/com/hanki/backend/controller/DeckControllerTest.java
+++ hanki-backend/src/test/java/com/hanki/backend/controller/DeckControllerTest.java
@@ -1,13 +1,19 @@
 package com.hanki.backend.controller;
 
+import com.hanki.backend.model.Deck;
+import com.hanki.backend.service.DeckService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,10 +23,29 @@ public class DeckControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    private DeckService deckService;
+
     @Test
     public void testGetAllDecks() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/decks"))
+        mockMvc.perform(get("/decks"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    public void testGetDeckById() throws Exception {
+        // Create mock Deck
+        int existingDeckId = 1;
+        Deck mockDeck = new Deck();
+        mockDeck.setId(existingDeckId);
+        mockDeck.setName("Mock Deck Name");
+        mockDeck.setDescription("Mock Deck Description");
+
+        // Mock the service method to return the mock Deck created
+        when(deckService.findById(existingDeckId)).thenReturn(Optional.of(mockDeck));
+
+        mockMvc.perform(get("/decks/" + existingDeckId))
+                .andExpect(status().isOk());
     }
 }
