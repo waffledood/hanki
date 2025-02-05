818ba80c714b1267cbc2f2862c0868279c252c71
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-05 21:42:13 +0800

    Define /decks/id API endpoint

--- hanki-backend/src/main/java/com/hanki/backend/controller/DeckController.java
+++ hanki-backend/src/main/java/com/hanki/backend/controller/DeckController.java
@@ -1,8 +1,10 @@
 package com.hanki.backend.controller;
 
+import com.hanki.backend.exception.DeckNotFoundException;
 import com.hanki.backend.model.Deck;
 import com.hanki.backend.service.DeckService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class DeckController {
     @GetMapping
     public Iterable<Deck> getAllDecks() {
         return deckService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Deck getDeckById(@PathVariable Integer id) {
+        return deckService.findById(id).orElseThrow(() -> new DeckNotFoundException("Deck not found with id: " + id));
     }
 }
