cf2f393ee423faabc0b51754b80be9ac5bb825ca
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-06 22:42:22 +0800

    Define DeckPostDto class

---
+++ hanki-backend/src/main/java/com/hanki/backend/dto/DeckPostDto.java
@@ -0,0 +1,35 @@
+package com.hanki.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class DeckPostDto {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 255, message = "Name must be at most 255 characters")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("name: %s, description: %s", this.name, this.description);
+    }
+}
