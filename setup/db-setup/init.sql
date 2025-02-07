08c2efe1e786f6e6e8acb53beafdc73d10baa367
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-07 23:29:38 +0800

    Update init.sql script to include creation of card table

--- setup/db-setup/init.sql
+++ setup/db-setup/init.sql
@@ -5,3 +5,15 @@ CREATE TABLE deck (
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL
 );
+
+-- Creation of card table
+
+CREATE TABLE card (
+    id SERIAL PRIMARY KEY,
+    front_text TEXT NOT NULL,
+    back_text TEXT NOT NULL,
+    deck_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_card_deck FOREIGN KEY (deck_id) REFERENCES deck(id) ON DELETE CASCADE
+);
