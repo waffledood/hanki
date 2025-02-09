ab6de386da27ad2fcc646f7b85c47e5ca76254fb
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-09 23:40:48 +0800

    Update CardPostDto

--- hanki-backend/src/main/java/com/hanki/backend/dto/CardPostDto.java
+++ hanki-backend/src/main/java/com/hanki/backend/dto/CardPostDto.java
@@ -1,4 +1,23 @@
 package com.hanki.backend.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
 public class CardPostDto {
+    @NotBlank(message = "Front text is required")
+    private String frontText;
+
+    @NotBlank(message = "Back text is required")
+    private String backText;
+
+    @NotNull(message = "Deck ID cannot be null")
+    @Min(value = 0, message = "Card ID must be at least 0")
+    private Integer deckId;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now();
 }
