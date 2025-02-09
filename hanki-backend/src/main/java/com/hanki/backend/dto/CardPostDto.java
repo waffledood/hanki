ce2bd6b2a01115675004cfac04e781c8ad5bae25
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-09 23:54:10 +0800

    Remove unnecessary createdAt & updatedAt variables in CardPostDto

--- hanki-backend/src/main/java/com/hanki/backend/dto/CardPostDto.java
+++ hanki-backend/src/main/java/com/hanki/backend/dto/CardPostDto.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDateTime;
-
 public class CardPostDto {
     @NotBlank(message = "Front text is required")
     private String frontText;
@@ -40,24 +38,4 @@ public class CardPostDto {
     public void setDeckId(Integer deckId) {
         this.deckId = deckId;
     }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    private LocalDateTime createdAt = LocalDateTime.now();
-
-    private LocalDateTime updatedAt = LocalDateTime.now();
 }
