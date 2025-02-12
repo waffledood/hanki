05d5bdecac89325fa70f3e8b1175f454a4938608
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-12 21:40:16 +0800

    Define test Spring profile in test classes

--- hanki-backend/src/test/java/com/hanki/backend/controller/CardControllerTest.java
+++ hanki-backend/src/test/java/com/hanki/backend/controller/CardControllerTest.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 @AutoConfigureMockMvc
 public class CardControllerTest {
     @Autowired
--- hanki-backend/src/test/java/com/hanki/backend/controller/DeckControllerTest.java
+++ hanki-backend/src/test/java/com/hanki/backend/controller/DeckControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 @AutoConfigureMockMvc
 public class DeckControllerTest {
 
