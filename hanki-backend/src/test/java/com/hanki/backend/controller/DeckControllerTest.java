0db029e429da1557092082b5943a22bb0c31a25c
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-02-12 21:42:08 +0800

    Change annotation from @Mock to @MockitoBean

--- hanki-backend/src/test/java/com/hanki/backend/controller/DeckControllerTest.java
+++ hanki-backend/src/test/java/com/hanki/backend/controller/DeckControllerTest.java
@@ -5,11 +5,11 @@ import com.hanki.backend.dto.DeckPostDto;
 import com.hanki.backend.model.Deck;
 import com.hanki.backend.service.DeckService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -17,9 +17,9 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
@@ -32,7 +32,7 @@ public class DeckControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Mock
+    @MockitoBean
     private DeckService deckService;
 
     @Test
