94cc9f9d271dddb748e1a2faf75c5c59c4c7b29b
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-03-08 08:57:19 +0800

    Define HankiUserDetailsService

---
+++ hanki-backend/src/main/java/com/hanki/backend/service/HankiUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.hanki.backend.service;
+
+import com.hanki.backend.model.UserPrincipal;
+import com.hanki.backend.model.User;
+import com.hanki.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HankiUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            // TODO - Implement logging
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
