60e42c996545fd3810ac57badfa59e51c5834a3a
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-03-15 10:37:39 +0800

    Return UserResponseDto object in /user/register response

--- hanki-backend/src/main/java/com/hanki/backend/controller/UserController.java
+++ hanki-backend/src/main/java/com/hanki/backend/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.hanki.backend.controller;
 
+import com.hanki.backend.dto.UserPostDto;
+import com.hanki.backend.dto.UserResponseDto;
 import com.hanki.backend.model.User;
 import com.hanki.backend.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +20,12 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping
-    public User registerUser(User user) {
-        return userService.registerUser(user);
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> registerUser(@Valid @RequestBody UserPostDto userPostDto) {
+        User user = userService.registerUser(userPostDto);
+
+        UserResponseDto userResponseDto = new UserResponseDto(user.getId(), user.getUsername(), user.getEmail());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDto);
     }
 }
