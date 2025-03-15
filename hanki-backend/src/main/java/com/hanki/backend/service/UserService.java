c5aed6d997ab68b17e1575cb16fe3994b87a4aba
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-03-15 10:11:55 +0800

    Define UserService

---
+++ hanki-backend/src/main/java/com/hanki/backend/service/UserService.java
@@ -0,0 +1,25 @@
+package com.hanki.backend.service;
+
+import com.hanki.backend.model.User;
+import com.hanki.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public User registerUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public PasswordEncoder getEncoder() {
+        return this.encoder;
+    }
+}
