91910b0d765830b40ac2356f48ed1c50f0075e42
waffledood <itshaikalhere@hotmail.sg>
2025-04-09 07:26:21 +0800

    Register Settings & Statistics routes

--- hanki-frontend/src/App.js
+++ hanki-frontend/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
-import Home from "./layout/Home";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Home from "./layout/Home";
+import Settings from "./pages/Settings";
+import Statistics from "./pages/Statistics";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/statistics" element={<Statistics />} />
       </Routes>
     </div>
   );
