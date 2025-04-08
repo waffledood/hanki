8731f2ca0d2ffeecb8b41147e124ba3801b7966d
waffledood <itshaikalhere@hotmail.sg>
2025-04-09 07:16:22 +0800

    Define App's routing structure

--- hanki-frontend/src/App.js
+++ hanki-frontend/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
 import Home from "./layout/Home";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
