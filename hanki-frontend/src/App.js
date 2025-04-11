b36701c56d2e0e52d076692580728519d149c0a4
waffledood <itshaikalhere@hotmail.sg>
2025-04-11 23:17:31 +0800

    Add DeckPage Route in App.js

--- hanki-frontend/src/App.js
+++ hanki-frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import DeckPage from "./pages/DeckPage";
 import Home from "./layout/Home";
 import Settings from "./pages/Settings";
 import Statistics from "./pages/Statistics";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/statistics" element={<Statistics />} />
+        <Route path="/decks/:deckId" element={<DeckPage />} />
       </Routes>
     </div>
   );
