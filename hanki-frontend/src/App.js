00e606e3a7fcf42021f3cb227f06fc1d0f5560f6
waffledood <itshaikalhere@hotmail.sg>
2025-04-13 17:09:23 +0800

    Add route for Login page

--- hanki-frontend/src/App.js
+++ hanki-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import DeckPage from "./pages/DeckPage";
 import Home from "./layout/Home";
+import LoginPage from "./pages/LoginPage";
 import Settings from "./pages/Settings";
 import Statistics from "./pages/Statistics";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/statistics" element={<Statistics />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/decks/:deckId" element={<DeckPage />} />
       </Routes>
     </div>
