3cd91994f8602482259fffa314fae9510883154b
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-04-18 21:24:55 +0800

    Add CORS in SecurityFilterChain

--- hanki-backend/src/main/java/com/hanki/backend/security/SecurityConfig.java
+++ hanki-backend/src/main/java/com/hanki/backend/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 // disable CSRF
                 .csrf(customizer -> customizer.disable())
                 // TODO - Update the exact endpoint(s) that don't require auth
