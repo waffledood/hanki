90782d9fc8d58efeb7011c1933e328222376e115
Haikal Yusuf <itshaikalhere@hotmail.sg>
2025-09-28 21:34:19 +0800

    fix(ui): change the icon for the study button on a Deck
    Change the icon from a text to a play icon

--- hanki-frontend/src/components/Deck.js
+++ hanki-frontend/src/components/Deck.js
@@ -44,8 +44,8 @@ function Deck({ id, name, totalCards, dueCards, progress, lastStudied }) {
             : "Not studied yet"}
         </span>
         <Link to={`/study/${id}`}>
-          <button className="!rounded-lg bg-indigo-600 text-white px-4 py-2 text-sm hover:bg-indigo-700 transition-colors duration-500 whitespace-nowrap">
-            Study Now
+          <button className="rounded-full bg-indigo-600 w-10 h-10 flex items-center justify-center hover:bg-indigo-700 transition-colors duration-500">
+            <i class="fa-solid fa-play text-white text-md"></i>
           </button>
         </Link>
       </div>
